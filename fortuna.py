72cb0f0c9a9f0fae05adfd9ad50aed9c7972a32f
Pedro da Costa Alves <pedroca10@al.insper.edu.br>
2022-11-12 19:23:57 -0300

    Adicionar  função valida_questao

--- fortuna.py
+++ fortuna.py
@@ -7,3 +7,48 @@ def transforma_base(lista):
             dicionario[exr['nivel']] = []
         dicionario[exr['nivel']].append(exr)
     return dicionario
+
+def valida_questao(questao):
+    erros = {}
+    # Verifiva se as chaves estão na questão
+    chaves = ['titulo','nivel','opcoes','correta']
+    for c in chaves:
+        if c not in questao:
+            erros[c] = 'nao_encontrado'
+    
+    #Verfica se a questao tem 4 chaves
+    if len(questao) != 4:
+        erros['outro'] = 'numero_chaves_invalido'
+    
+    #Verifica chave titulo
+    if 'titulo' in questao.keys():
+        if questao['titulo'].strip() == '':
+            erros['titulo'] = 'vazio'
+
+    #Verifica a chave nivel
+    if 'nivel' in questao.keys():
+        niveis = ['facil','medio','dificil']
+        if questao['nivel'] not in niveis:
+            erros['nivel'] = 'valor_errado' 
+
+    #Verifica quantidade de opcoes
+    if 'opcoes' in questao.keys():
+        if len(questao['opcoes']) != 4:
+            erros['opcoes'] = 'tamanho_invalido'
+        else:
+    #Verifica opcoes tenha 4 chaves
+            letras = ['A','B','C','D']
+            for l in questao['opcoes'].keys():
+                if l not in questao['opcoes']:
+                    erros['opcoes'] = 'chave_invalida_ou_nao_encontrada'
+                else:
+                    if questao['opcoes'][l].strip() == '':
+                        if 'opcoes' not in erros:
+                            erros['opcoes'] = {}
+                        erros['opcoes'][l] = 'vazia'
+    
+    if 'correta' in questao.keys():
+        letras = ['A','B','C','D']
+        if questao['correta'] not in letras:
+            erros['correta'] = 'valor_errado'
+    return erros 
